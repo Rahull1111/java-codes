f40280ce7cc0d92f3cf368a2cb4cad97f6cf9b2e
Rahull1111 <rahulnitharwal1111@gmail.com>
2025-07-31 22:15:59 +0530

    first commit

--- recursionExample.java
+++ recursionExample.java
@@ -1,3 +1,21 @@
 public class recursionExample {
+   static int factorial(int n){
+
+    if(n==0 || n==1){
+        return 1;
+    }
+    else{
+
+       int product = 1;
+       for(int i=1;i<=n; i++){
+        product *= 1;
+       }
+       return product;
+    }
+   } 
+   public static void main(String[] args) {
+    int n = 0;
+    System.out.println("the value of factorial n is " +factorial(n));
     
+   }
 }
